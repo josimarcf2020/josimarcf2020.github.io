8a4159563f50879fc6faf5b083e2b17fea2390e7
Josimar Ferreira <jcfcavile@gmail.com>
2025-04-16 09:20:55 -0300

    ajustes finais

--- detalhamentos.js
+++ detalhamentos.js
@@ -2,18 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const detalhamento = document.getElementById('detalhamento');
 
     txtHabilidades = '<h3>Habilidades Técnicas</h3>';
-    txtHabilidades += '<ul><li><strong>Desenvolvimento de Aplicativos iOS</strong>: Proficiência em desenvolvimento para dispositivos iOS utilizando Swift e SwiftUI, com experiência em ferramentas como Xcode.</li>';
-    txtHabilidades += '<li><strong>Gestão de Interface (Front-end)</strong>: Criação de views alinhadas a especificações de design, conectando-as a modelos via ViewModels (VMC).</li>';
-    txtHabilidades += '<li><strong>Desenvolvimento de APIs</strong>: Criação e integração de APIs para consumo no backend, demonstrando conhecimento em comunicação entre front-end e back-end.</li>';
+    txtHabilidades += '<ul><li><strong>Desenvolvimento de Aplicativos iOS</strong>: Proficiência em desenvolvimento para dispositivos iOS utilizando Swift, SwiftUI e Xcode.</li>';
+    txtHabilidades += '<li><strong>Gestão de Interface (Front-end)</strong>: Criação de views alinhadas a especificações de design, conectando-as via ViewModels (VMC).</li>';
+    txtHabilidades += '<li><strong>Desenvolvimento de APIs</strong>: Criação e integração de APIs.</li>';
     txtHabilidades += '<li><strong>Controle de Versão</strong>: Uso avançado de Git e GitHub para colaboração em equipe e versionamento de código.</li>';
-    txtHabilidades += '<li><strong>Banco de Dados</strong>: Experiência com Core Data, MongoDB, Firebase e bancos relacionais como MySQL, aplicados em diferentes projetos.</li>';
+    txtHabilidades += '<li><strong>Banco de Dados</strong>: Experiência com Core Data, MongoDB, Firebase e bancos relacionais como MySQL.</li>';
     txtHabilidades += '<li><strong>Frameworks e Bibliotecas</strong>: Utilização de Vapor (framework back-end em Swift) e outras tecnologias modernas para desenvolvimento full-stack.</li>';
     txtHabilidades += '<li><strong>Programação Legada</strong>: Conhecimento em linguagens como Clipper, Delphi e xHarbour para sistemas de gestão e suporte.</li>';
-    txtHabilidades += '<li><strong>Integração de Sistemas</strong>: Habilidade em conectar front-end e back-end, além de processar arquivos de movimentação bancária para baixa de parcelas.</li>';
+    txtHabilidades += '<li><strong>Integração de Sistemas</strong>';
     txtHabilidades += '</ul>';
     txtHabilidades += '<h3>Habilidades Funcionais</h3>';
     txtHabilidades += '<ul><li><strong>Gestão de Projetos</strong>: Desenvolvimento de soluções como o CalmDesk e fProject, voltadas para gestão de demandas e comunicação em ambientes específicos.</li>';
-    txtHabilidades += '<li><strong>Suporte de TI</strong>: Resolução de incidentes e suporte técnico em sistemas empresariais, como na Cooperativa Mista dos Avicultores do Piauí.</li>';
     txtHabilidades += '<li><strong>Automação de Processos</strong>: Criação de sistemas como o ADMESC, abrangendo controle financeiro, frequência escolar e emissão de boletins.</li>';
     txtHabilidades += '<li><strong>Análise de Requisitos</strong>: Colaboração com designers e equipes multifuncionais para traduzir especificações em soluções técnicas.</li>';
     txtHabilidades += '</ul>';
@@ -26,14 +25,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     txtExperiencia =  '<h3>Experiência Profissional</h3>';
     txtExperiencia += '<ul>';
-    txtExperiencia += ' <li><strong>Residente - Projeto Restic 12 Instituto iRede - Jul de 2024 - Dez de 2024</strong> <a href="./doc/Certificado JOSIMAR DA CUNHA FERREIRA - Residencia TIC 12.pdf" target="_blank">Certificado</a>';
+    txtExperiencia += ' <li><strong>Residente - Projeto Restic 12 Instituto iRede</strong> <a href="./doc/Certificado JOSIMAR DA CUNHA FERREIRA - Residencia TIC 12.pdf" target="_blank">Certificado</a>';
     txtExperiencia += ' <ul>';
     txtExperiencia += '     <li>Desenvolvimento de aplicativos para dispositivos iOS utilizando Swift, framework SwiftUI, xCode.';
-    txtExperiencia += '     <li>Colaboração com equipes multifuncionais para criar views seguindo definições estabelecidas pelos designers, utilizando GIT/GITHUB.';
+    txtExperiencia += '     <li>Colaboração com equipes multifuncionais utilizando GIT/GITHUB.';
     txtExperiencia += '     <li>Codificação de rotinas para conectar as views aos modelos através de ViewModels VMC.';
-    txtExperiencia += '     <li>Desenvolvimento de API para consumo no backend.';
-    txtExperiencia += '     <li>Integração Front e Backend.</li>';
-    txtExperiencia += '     <li>Desenvolvimento de API para consumo no backend.';
+    txtExperiencia += '     <li>Desenvolvimento de API';
     txtExperiencia += '     <li>Integração Front e Backend.</li>';
     txtExperiencia += '     <li>Projetos desenvolvidos:</li>';
     txtExperiencia += '     <ul>';
@@ -51,11 +48,11 @@ document.addEventListener('DOMContentLoaded', () => {
     txtExperiencia += '         </ul>';
     txtExperiencia += '     </ul>';
     txtExperiencia += '  </ul>';
-    txtExperiencia += '  <li><strong>Programador Júnior - Cooperativa Mista dos Avicultores do Piauí | Jan 2007 - Abr 2007</strong>';
+    txtExperiencia += '  <li><strong>Programador Júnior xHarbour - Cooperativa Mista dos Avicultores do Piauí</strong>';
     txtExperiencia += '  <ul>';
     txtExperiencia += '      <li>Desenvolvimento de Sistema de TI, prestando atendimento aos incidentes gerados pelo uso do sistema adotado pela empresa. Linguagem xHarbour, Delphi, SGDB MySql.';
     txtExperiencia += '  </ul>';
-    txtExperiencia += '  <li><strong>Programador Senior - AESPI Assoc de Ensino Superior do Piauí LTDA | Ago 1996 - Abr 2000</strong>';
+    txtExperiencia += '  <li><strong>Programador Senior Clipper - AESPI Assoc de Ensino Superior do Piauí LTDA</strong>';
     txtExperiencia += '  <ul>';
     txtExperiencia += '      <li>Desenvolvimento de Sistema de Gestão Escolar ADMESC, em linguagem Clipper, envolvendo desde o controle e acesso (frequencia escolar) via catraca eletrônica, controle financeiro, controle de notas e faltas, emissão de boletim escolar, realização de teste admissional escolar (teste seletivo), baixa de parcelas através de processamento de arquivo de movimentação bancária, e demais tarefas pertinentes e rotina escolar.</li>';
     txtExperiencia += '      <li>Linguagem Clipper, Delphi, banco de dados relacional.</li>';
@@ -67,7 +64,7 @@ document.addEventListener('DOMContentLoaded', () => {
     txtEducacao += '<ul>';
     txtEducacao += ' <li><strong>Técnico em Desenvolvimento de Sistemas - SENAI</strong>, Fortaleza-CE centro | 2023 <a href="./doc/000168 - JOSIMAR DA CUNHA FERREIRA Diplona SENAI-Assinado.pdf" target="_blank">Certificado</a></li>';
     txtEducacao += ' <li><strong>Técnico em Processamento de Dados - Escola Téc Est Pref. João Mendes Olímpio de Melo</strong> - Teresina Piauí | Mar 1993 - Nov 1995 <a href="./doc/Certificado PD Josimar Ferreira.pdf" target="_blank">Certificado</a></li>';
-    txtEducacao += ' <li><strong>Graduação: Análise e Desenvolvimento de Sistemas - Faculdade CDL</strong> Fortaleza-CE TRANCADO 3º Periodo</li>';
+    txtEducacao += ' <li><strong>Graduação: Análise e Desenvolvimento de Sistemas - Faculdade CDL</strong> Fortaleza-CE 3º Periodo</li>';
     txtEducacao += '</ul>';
 
     txtSobreMim = '<h3>Sobre mim</h3>';
